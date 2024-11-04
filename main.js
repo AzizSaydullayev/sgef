130c2c6e8981f521e68c7fbce28460db3a57cd11
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-11-04 16:37:44 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,62 @@
 let obj = {
     id: "777",
     password: "701",
-}
+};
 
-let login = document.querySelector('.login')
-let parol = document.querySelector('.parol')
-const sendBtn = document.querySelector('.send-btn')
-const validation = document.querySelector('.validation')
-let inputFirst = ''
-let inputSecond = ''
+let modmeId = document.querySelector(".login");
+let modmePassword = document.querySelector(".parol");
+const sendBtn = document.querySelector(".send-btn");
+const form = document.querySelector("form")
+let validation = document.querySelector(".validation")
 
-login.addEventListener('input', e => {
-    inputFirst = e.target.value
-    show()
-})
+let inputFirst = "";
+let inputSecond = "";
 
-parol.addEventListener('input', e => {
-    inputSecond = e.target.value
-    show()
-})
+sendBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(modmeId.value);
+    console.log(modmePassword.value);
+});
+
+modmeId.addEventListener("input", (e) => {
+    inputFirst = e.target.value;
+    show();
+});
+
+modmePassword.addEventListener("input", (e) => {
+    inputSecond = e.target.value;
+    show();
+});
 
 const show = () => {
     if (inputFirst !== "" && inputSecond !== "") {
-        sendBtn.classList.add('show')
+        sendBtn.classList.add("show");
     } else {
-        sendBtn.classList.remove('show')
+        sendBtn.classList.remove("show");
     }
-}
+};
 
-sendBtn.addEventListener('click', e => {
+
+sendBtn.addEventListener("click", e => {
     e.preventDefault()
-    if (login.value !== '' && parol.value !== '') {
-        let newObj = {
-            id: login.value,
-            password: parol.value
-        }
-        if (obj.id === newObj.id && obj.password === newObj.password) {
-            window.location.href = './HomePage.html'
-            localStorage.setItem('token', JSON.stringify(newObj))
+
+    if (modmeId.value != "" && modmePassword.value != "") {
+        validation.innerHTML = ""
+
+        let newObj = {}
+        newObj.id = modmeId.value
+        newObj.password = modmePassword.value
+        if (obj.id == newObj.id && obj.password == newObj.password) {
+            window.location.href = "./HomePage.html"
+            localStorage.setItem("token",JSON.stringify(newObj))
         } else {
-            alert("Bunday user yo'q")
+            alert("Bunday user yoq")
         }
+
     } else {
-        validation.textContent = "Bo'sh joylarni to'ldiring!"
+        validation.textContent = "Bosh joylarni toldiring"
     }
 
-    login.value = ""
-    parol.value = ""
+    modmeId.value=""
+    modmePassword.value=""
 })
